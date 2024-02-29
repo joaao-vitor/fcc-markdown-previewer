d07c62ecc12e1d3814a25036c5e51dac970f458b
João Vitor <jvitor.arrudal@gmail.com>
2024-02-29 14:52:39 -0300

    vite for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://joaao-vitor.github.io/fcc-markdown-previewer/',
   plugins: [react()],
 })
